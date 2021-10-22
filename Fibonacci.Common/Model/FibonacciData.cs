59095971e2540b36dab553fc887c366652f071cd
Артем Петров <petrovas@osib.so-ups.ru>
2021-10-22 04:23:43 +0300

    Comments altered

--- Fibonacci.Common/Model/FibonacciData.cs
+++ Fibonacci.Common/Model/FibonacciData.cs
@@ -6,7 +6,7 @@
         public string SessionId { get; set; }
 
 
-        //TODO: Int32 will overflow very fast, replace with Int32 with BigNumber 
+        //TODO: Int32 will overflow very fast, replace Int32 with BigNumber 
         [System.Text.Json.Serialization.JsonPropertyName("niValue")]
         public int NiValue { get; set; }
     }
